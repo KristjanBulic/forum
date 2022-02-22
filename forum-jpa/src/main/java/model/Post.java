14348077a78559abc01e1af45513d3ee8a5c29a9
Kristjan Bulič <kristjan.bulic@result.si>
2022-02-22 21:27:40 +0100

    runs

--- forum-jpa/src/main/java/model/Post.java
+++ forum-jpa/src/main/java/model/Post.java
@@ -1,10 +1,32 @@
 package model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import lombok.Data;
 
-//@Entity
-//@Table("T_POST")
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "T_POST")
 public class Post {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Integer id;
+
+    @Column(name = "TITLE")
+    private String title;
+
+    @Column(name = "TEXT")
+    private String text;
+
+    @Column(name = "POST_TIME")
+    private Date postTime;
+
+    @Column(name = "FILE_URL")
+    private String fileUrl;
+
+    @Column(name = "USER_ID")
+    private Integer userId;
 }

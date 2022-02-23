2ce1f63d060844daadda8482863052f1ab4e951c
Kristjan Bulič <kristjan.bulic@result.si>
2022-02-23 11:21:26 +0100

    docker-compose

---
+++ forum-jpa/src/main/java/model/Comment.java
@@ -0,0 +1,13 @@
+package model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "T_COMMENT")
+public class Comment {
+
+}

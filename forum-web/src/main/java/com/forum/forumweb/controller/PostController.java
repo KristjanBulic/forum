e52dd29563c05c0f2fa63df210dae644d976caa0
Kristjan Bulič <kristjan.bulic@result.si>
2022-03-16 18:52:00 +0100

    openapi

--- forum-web/src/main/java/com/forum/forumweb/controller/PostController.java
+++ forum-web/src/main/java/com/forum/forumweb/controller/PostController.java
@@ -3,7 +3,6 @@ package com.forum.forumweb.controller;
 import com.forum.forumweb.model.Post;
 import com.forum.forumweb.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;

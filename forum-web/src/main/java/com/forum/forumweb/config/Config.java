3ede76c10781a4a4bd2df81f2c9a549ce2dc72e4
Kristjan Bulič <kristjan.bulic@result.si>
2022-05-02 17:20:39 +0200

    lol lol

--- forum-web/src/main/java/com/forum/forumweb/config/Config.java
+++ forum-web/src/main/java/com/forum/forumweb/config/Config.java
@@ -3,6 +3,7 @@ package com.forum.forumweb.config;
 import net.kaczmarzyk.spring.data.jpa.web.SpecificationArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -13,6 +14,12 @@ public class Config implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(new SpecificationArgumentResolver());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS").allowedOrigins("*");
     }
 
 }
